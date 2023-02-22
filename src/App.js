5dfe1f0e9bd80e7b0ca221141b655cbfb3155081
Dmitriy K <d.komerzan@gmail.com>
2023-02-22 14:14:18 -0500

    deployment number 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>this is a random test deployment</p>
+        <p>change for deployment number 3</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
