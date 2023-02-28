1ff39a1121dd5598b5657bb1936aab6ebe6b02ae
Dmitriy K <d.komerzan@gmail.com>
2023-02-28 16:15:07 -0500

    added graphQL

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dmitriytestdeploymen": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "dmitriytestdeploymen453ee5bb": {
       "AppClientID": "string",
